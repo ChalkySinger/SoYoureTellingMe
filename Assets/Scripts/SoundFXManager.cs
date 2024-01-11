79d1ffafd23f729339441181441022680ab56b34
RanFernaj <rf4958o@gre.ac.uk>
2024-01-11 21:35:37 +0000

    Update SoundFXManager.cs
    Added Audio clip arrays, Added Functions for playing random sfx from an array of sfx

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -6,7 +6,21 @@ public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
 
+    public enum SoundFXTypes 
+    {
+        HobDial, Sizzle, Chopping, BottlePops, EggCrack
+    }
+
     [SerializeField] AudioSource soundFXClip;
+    [Header("General SFX")]
+    [SerializeField] AudioClip hobDial;
+    [SerializeField] AudioClip[] sizzle;
+
+    [Header("Ingredient SFX")]
+    [SerializeField] AudioClip[] chopping;
+    [SerializeField] AudioClip[] bottlePops;
+    [SerializeField] AudioClip eggCrack;
+
 
     private void Awake()
     {
@@ -16,9 +30,9 @@ public class SoundFXManager : MonoBehaviour
         }
     }
 
-    public void PlaySoundFX(AudioClip audioClip, Transform spawnPoint)
+    public void PlaySoundFX(AudioClip audioClip, Vector3 spawnPoint)
     {
-        AudioSource audioSource = Instantiate(soundFXClip, spawnPoint.position, Quaternion.identity);
+        AudioSource audioSource = Instantiate(soundFXClip, spawnPoint, Quaternion.identity);
 
         audioSource.clip = audioClip;
 
@@ -29,5 +43,44 @@ public class SoundFXManager : MonoBehaviour
         float audioLength = audioSource.clip.length;
 
         Destroy(audioSource, audioLength);
+    }
+
+    public void PlayRandomSoundFX(AudioClip[] audioClip, Vector3 spawnPoint)
+    {
+        int rand = Random.Range(0, audioClip.Length);
+
+        AudioSource audioSource = Instantiate(soundFXClip, spawnPoint, Quaternion.identity);
+
+        audioSource.clip = audioClip[rand];
+
+        audioSource.volume = PlayerPrefs.GetFloat("VolumeValue");
+
+        audioSource.Play();
+
+        float audioLength = audioSource.clip.length;
+
+        Destroy(audioSource, audioLength);
+    }
+
+    public void AudioTrigger(SoundFXTypes audioType, Vector3 spawnPoint)
+    {
+        switch(audioType)
+        {
+            case SoundFXTypes.HobDial:
+                PlaySoundFX(hobDial, spawnPoint);
+                break;
+            case SoundFXTypes.BottlePops:
+                PlayRandomSoundFX(bottlePops, spawnPoint);
+                break;
+            case SoundFXTypes.Chopping:
+                PlayRandomSoundFX(chopping, spawnPoint);
+                break;
+            case SoundFXTypes.Sizzle:
+                PlayRandomSoundFX(sizzle, spawnPoint);
+                break;
+            case SoundFXTypes.EggCrack:
+                PlaySoundFX(eggCrack, spawnPoint);
+                break;
+        }
     }
 }
